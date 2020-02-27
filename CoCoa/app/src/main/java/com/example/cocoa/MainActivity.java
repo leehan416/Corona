4199efc67b4924c43e9691384f5738663799a191
KIS02 <rladlstjd87@gmail.com>
2020-02-27 23:30:48 +0900

    Fix

--- CoCoa/app/src/main/java/com/example/cocoa/MainActivity.java
+++ CoCoa/app/src/main/java/com/example/cocoa/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.cocoa;
 
-import com.example.cocoa.R;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -19,10 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     String Url = "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=&brdGubun=&ncvContSeq=&contSeq=&board_id=&gubun=";
 
-    String list = "";
-
     String Url2 = "https://www.coronanow.kr/";
 
+    String list = "";
 
     TextView Today;
     TextView Today_1;
@@ -51,12 +48,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void DataLoad() {
         list = "";
+        list2 = "";
         JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
         jsoupAsyncTask.execute();
     }
 
-
     String list2 = "";
+
     private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
         @Override
         protected void onPreExecute() {
@@ -67,16 +65,16 @@ public class MainActivity extends AppCompatActivity {
         protected Void doInBackground(Void... params) {
             try {
                 Document doc = Jsoup.connect(Url).get();
-                Elements titles = doc.select("ul[class=s_listin_dot]").eq(0);
+                Elements titles = doc.select("table[class=num]").eq(0);
                 for (Element e : titles) {
-                    list = e.select("li").text().trim() + "\n";
+                    list = e.text().trim() + "\n";
                 }
+
                 doc = Jsoup.connect(Url2).get();
-                titles = doc.select("div[class=card-body2]") .eq(1)   ;
+                titles = doc.select("div[class=card-body2]").eq(1);
                 for (Element e : titles) {
                     list2 += e.text().trim();
                 }
-
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -88,19 +86,20 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(Void result) {
             String[] tmp = new String[5];
 
+
             tmp = list.split("명");
 
-            tmp[0] = tmp[0].substring(7, tmp[0].length());
-            tmp[1] = tmp[1].substring(12, tmp[1].length());
-            tmp[2] = tmp[2].substring(6, tmp[2].length());
-            tmp[3] = tmp[3].substring(7, tmp[3].length());
-
+            tmp[0] = tmp[0].substring(49, tmp[0].length()).trim();
+            tmp[1] = tmp[1].substring(11, tmp[1].length()).trim();
+            tmp[2] = tmp[2].substring(5, tmp[2].length()).trim();
+            tmp[3] = tmp[3].substring(6, tmp[3].length()).trim();
+            tmp[4] = ""; // 이거가 오늘 숫자
 
             Infected.setText("확진환자\n"+ tmp[0] +"명");
             Dead.setText("격리해제\n"+ tmp[1] +"명");
             Suspected.setText("사망자\n"+tmp[2] +"명");
             Test.setText("검사진행\n" + tmp[3] +"명");
-            tmp[4] = ""; // 이거가 오늘 숫자
+
             boolean b = false;
             String temp = "";
             for( int i = 0; i < list2.length(); i++){
