1c5f75069be0516095292bd3a5443d44a1ee6198
KIS02 <rladlstjd87@gmail.com>
2020-03-01 23:31:49 +0900

    a

--- CoCoa/app/src/main/java/com/example/cocoa/MainActivity.java
+++ CoCoa/app/src/main/java/com/example/cocoa/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cocoa;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.ViewDebug;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -17,9 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
     String Url = "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=&brdGubun=&ncvContSeq=&contSeq=&board_id=&gubun=";
 
-    String Url2 = "https://www.coronanow.kr/";
+    String Url2 = "http://www.coronanow.kr/";
 
     String list = "";
+    String list2 = "";
 
     TextView Today;
     TextView Today_1;
@@ -49,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         try {
         list = "";
         list2 = "";
+
         JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
         jsoupAsyncTask.execute();
         } catch (Exception p){
@@ -56,8 +59,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-
-    String list2 = "";
 
     private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
         @Override
@@ -69,9 +70,10 @@ public class MainActivity extends AppCompatActivity {
         protected Void doInBackground(Void... params) {
             try {
                 Document doc = Jsoup.connect(Url).get();
-                Elements titles = doc.select("table[class=num]").eq(0);
+                Elements titles = doc.select("table.num").eq(0).select("tbody");//.select("td[class=w_bold]");
+
                 for (Element e : titles) {
-                    list = e.text().trim() + "\n";
+                    list += e.select("td").text().trim() + "\n";
                 }
 
                 doc = Jsoup.connect(Url2).get();
@@ -88,21 +90,23 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Void result) {
-            String[] tmp;
+            String[] tmp = {"",};
             String s = "";
 
-            tmp = list.split("명");
-
-            tmp[0] = tmp[0].substring(49, tmp[0].length()).trim();
-            tmp[1] = tmp[1].substring(11, tmp[1].length()).trim();
-            tmp[2] = tmp[2].substring(5, tmp[2].length()).trim();
-            tmp[3] = tmp[3].substring(6, tmp[3].length()).trim();
-
+            try {
+                tmp = list.split("명");
+                tmp[0] = tmp[0].substring(5, tmp[3].length()).trim();
+                tmp[1] = tmp[1].substring(9, tmp[1].length()).trim();
+                tmp[2] = tmp[2].substring(4, tmp[2].length()).trim();
+                tmp[3] = tmp[3].substring(5, tmp[3].length()).trim();
+            } catch (Exception e){
+            }
 
             Infected.setText("확진환자\n"+ tmp[0] +"명");
             Dead.setText("격리해제\n"+ tmp[1] +"명");
             Suspected.setText("사망자\n"+tmp[2] +"명");
             Test.setText("검사진행\n" + tmp[3] +"명");
+
 
             boolean b = false;
             String temp = "";
